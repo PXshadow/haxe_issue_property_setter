56311e1367badb76896511332818baa1cf173baa
PXshadow <joeyprather8@gmail.com>
2020-12-02 00:44:22 -0800

    minimize boiler plate

--- src/Main.hx
+++ src/Main.hx
@@ -1,7 +1,7 @@
 class Main {
 	static function main() {
 		var x = 5;
-		var data = new Data(() -> x,(y) -> x = y);
+		var data = new Data();
 		trace("x: " + Macro.test(data));
 		Macro.test(data) = 4;
 		trace("x2: " + Macro.test(data));
@@ -11,17 +11,12 @@ class Data {
 	public var value(get, set):Int;
 	
 	function get_value():Int {
-		return get();
+		return 0;
 	}
 	
 	function set_value(value:Int):Int {
-		set(value);
 		return value;
 	}
-	var get:Void->Int;
-	var set:Int->Void;
-	public function new(get,set) {
-		this.set = set;
-		this.get = get;
-	}
+	public function new() {}
+	
 }
